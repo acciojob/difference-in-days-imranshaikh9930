87ec87b7f49a8ad6abb759d20c3e90b26407e896
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-14 13:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const utcDate1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    const utcDate2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+    // Calculate the difference in days
+    const millisecondsInDay = 24 * 60 * 60 * 1000;
+    const differenceInDays = Math.floor((utcDate2 - utcDate1) / millisecondsInDay);
+
+    return differenceInDays;
 };
 
 // Do not change the code below.
