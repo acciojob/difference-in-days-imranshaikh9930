80c24a787b2aa73ff148f63ef8ae7c091d0afd59
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-21 14:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-	const utcDate1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
-    const utcDate2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+    // Convert input strings to Date objects
+    const parsedDate1 = new Date(date1);
+    const parsedDate2 = new Date(date2);
+
+    // Convert to UTC dates
+    const utcDate1 = Date.UTC(parsedDate1.getFullYear(), parsedDate1.getMonth(), parsedDate1.getDate());
+    const utcDate2 = Date.UTC(parsedDate2.getFullYear(), parsedDate2.getMonth(), parsedDate2.getDate());
 
     // Calculate the difference in days
     const millisecondsInDay = 24 * 60 * 60 * 1000;
@@ -11,6 +15,6 @@ var dateDiffInDays = function (date1, date2) {
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+const dateOne = prompt("Enter Start Date (YYYY-MM-DD):");
+const dateTwo = prompt("Enter End Date (YYYY-MM-DD):");
 alert(dateDiffInDays(dateOne, dateTwo));
